8fcf4763367f4e83d0fdb4fce8ba47b653b39ca4
Maria Bampa <maria.bampa@dsv.su.se>
2022-03-22 15:40:48 +0100

    clutering pt1

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-import pandas as pd, numpy as np, datetime as dt
+import pandas as pd, numpy as np, datetime as dt, matplotlib.pyplot as plt 
 
 
 
@@ -19,6 +19,30 @@ def str2time(val):
 
 
 
+def drop_recordings(df, column_name_, threshold):
+    """
+    drop columns with recordings<threshold
+    input -- 
+        df: pandas df
+        column_name: string, the name of multiindex column on level 1
+        threshold
+    output -- 
+        df_red: pd.df, reduced df column 
+    
+    """
+    threshold = 0.9
+    columns_to_drop = []
+    for column_name in df.columns:
+        #print(column_name)
+        if column_name[1] == column_name_:
+            #get the only the means of the vital_labs
+            if df[column_name].isnull().mean() > threshold:
+                columns_to_drop.append(column_name[0])
+    df_red = df.drop(columns=columns_to_drop)
+    return df_red
+
+
+
 # From MIMIC extract paper
 def get_variable_mapping(mimic_mapping_filename):
     # Read in the second level mapping of the itemids
@@ -32,6 +56,24 @@ def get_variable_mapping(mimic_mapping_filename):
 
     return var_map
 
+
+
+
+
+def get_variable_ranges(range_filename):
+    # Read in the second level mapping of the itemid, and take those values out
+    columns = [ 'LEVEL2', 'OUTLIER LOW', 'VALID LOW', 'IMPUTE', 'VALID HIGH', 'OUTLIER HIGH' ]
+    to_rename = dict(zip(columns, [ c.replace(' ', '_') for c in columns ]))
+    to_rename['LEVEL2'] = 'VARIABLE'
+    var_ranges = pd.read_csv(range_filename, index_col=None)
+    var_ranges = var_ranges[columns]
+    var_ranges.rename(columns=to_rename, inplace=True)
+    var_ranges = var_ranges.drop_duplicates(subset='VARIABLE', keep='first')
+    var_ranges['VARIABLE'] = var_ranges['VARIABLE'].str.lower()
+    var_ranges.set_index('VARIABLE', inplace=True)
+    var_ranges = var_ranges.loc[var_ranges.notnull().all(axis=1)]
+
+    return var_ranges
 
 def apply_variable_limits(df, var_ranges, var_names_index_col='LEVEL2'):
     idx_vals        = df.index.get_level_values(var_names_index_col)
@@ -96,19 +138,19 @@ def get_values_by_name_from_df_column_or_index(data_df, colname):
             values = data_df.index.get_level_values(colname)
         else:
             raise e
-    return 
+    return values
 
 
     
 UNIT_CONVERSIONS = [
     ('weight',                   'oz',  None,             lambda x: x/16.*0.45359237),
     ('weight',                   'lbs', None,             lambda x: x*0.45359237),
-    ('fraction inspired oxygen', None,  lambda x: x > 1,  lambda x: x/100.),
-    ('oxygen saturation',        None,  lambda x: x <= 1, lambda x: x*100.),
-    ('temperature',              'f',   lambda x: x > 79, lambda x: (x - 32) * 5./9),
+    ('Fraction inspired oxygen', None,  lambda x: x > 1,  lambda x: x/100.),
+    ('Oxygen saturation',        None,  lambda x: x <= 1, lambda x: x*100.),
+    ('Temperature',              'f',   lambda x: x > 79, lambda x: (x - 32) * 5./9),
     ('height',                   'in',  None,             lambda x: x*2.54),
 ]
-def standardize_units(X, name_col='itemid', unit_col='valueuom', value_col='value', inplace=True):
+def standardize_units(X, name_col='LEVEL2', unit_col='valueuom', value_col='value', inplace=True):
     if not inplace: X = X.copy()
     name_col_vals = get_values_by_name_from_df_column_or_index(X, name_col)
     unit_col_vals = get_values_by_name_from_df_column_or_index(X, unit_col)
@@ -142,7 +184,6 @@ def standardize_units(X, name_col='itemid', unit_col='valueuom', value_col='valu
 
     return X
 
-
 def range_unnest(df, col, out_col_name=None, reset_index=False):
     assert len(df.index.names) == 1, "Does not support multi-index."
     if out_col_name is None: out_col_name = col
@@ -156,7 +197,27 @@ def range_unnest(df, col, out_col_name=None, reset_index=False):
     return col_flat
 
 
-import matplotlib.pyplot as plt 
+def print_proportions(df):
+    """
+    # Print the total proportions!
+
+    input: pandas df
+    
+    """
+
+
+    rows, vars = df.shape
+    print('')
+    for l, vals in df.iteritems():
+        if str(l[1]) == 'mean':
+            ratio = 1.0 * vals.dropna().count() / rows
+            print(str(l) + ': ' + str(round(ratio, 3)*100) + '% present')
+
+    return None 
+
+
+
+
 def plot_variable_histograms(col_names, df):
     # Plot some of the data, just to make sure it looks ok
     for c, vals in df.iteritems():
@@ -180,7 +241,7 @@ def plot_variable_histograms(col_names, df):
         plt.plot([], label='Variance: {}'.format(format(var,'.2f')),
                  color='lightgray')
         plt.plot([], label='Skew: {}'.format(format(skew,'.2f')),
-                 color='light:gray')
+                 color='lightgray')
 
         # add title, labels etc.
         plt.title('{} measurements in ICU '.format(str(c)))
